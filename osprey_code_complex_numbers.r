a2d51f976d696e67102dd7ffaafbcd1afb97b3fb
FedericoTossani <fede.tossani@gmail.com>
2023-06-09 13:25:40 +0200

    Create osprey_code_complex_numbers.r

---
+++ osprey_code_complex_numbers.r
@@ -0,0 +1,166 @@
+
+# ------------------------------------------------ #       
+#         Ranging behavior of non-breeding
+#         Mediterranean ospreys tracked by
+#         GPS-satellite telemetry
+# ------------------------------------------------ #
+
+
+
+# Relatore: Prof. Duccio Rocchini
+# Correlatori: Dott. Francesco Pezzo
+#              Dott. Flavio Monti
+#              Dott. Andrea Sforzi
+# Univesity: Alma Mater Studiorum University of Bologna
+
+
+
+# =========================== #
+         ## SUMMARY ##
+# =========================== #
+
+# 1. SetWD and packages
+# 2. Data import
+# 3. Filtering data
+# 4. Processing data
+# 5. 
+# 6. 
+
+# ================================ #
+#       1.SetWD and packages       #
+# ================================ #
+         setwd("C:/Tesi/R/osprey/data/")
+
+         list.of.packages <- c("tidyverse",
+                               "lubridate",
+                               "sf", 
+                               "mapview", 
+                               "adehabitatLT",
+                               "adehabitatHR",
+                               "stargazer",
+                               "kableExtra",
+                               "ggmap",
+                               "terra",
+                               "tidyterra",
+                               "gridExtra",
+                               "ggpubr")
+
+         # with this line of code I check if alll the packages are installed and then I load it
+
+         {
+           new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+           if(length(new.packages)) install.packages(new.packages)
+
+           lapply(list.of.packages, require, character.only = TRUE)
+         }
+
+# ================================= #
+#          2.Data import            #
+# ================================= #
+
+         list_csv <- list.files(pattern = "20")
+         csv_allfile <- lapply(list_csv, read.csv)
+
+         col_selected <- c("timestamp", "location.long", "location.lat", "external.temperature", 
+                           "gsm.gsm.signal.strength", "sensor.type", "individual.local.identifier")
+
+         csv_file_sel_col <- lapply(csv_allfile, "[", , col_selected)
+         osprey_raw <- bind_rows(csv_file_sel_col)
+
+         osprey_death <- read.csv("osprey_death.csv")
+
+
+# ================================= #
+#        3. Filtering data          #
+# ================================= #
+
+# dead ospreys
+
+         osprey_dead <- c("Balearics2013_FOSP11-Juv_ringH7", "Corsica2013_FOSP17_Juv_ringCBK",
+                          "Italy2017_FOSP43_juv_ringIBI_Agrippa", "Italy2018_FIOS45_juv_ringIAB_Anassagora")
+
+# dataset to use for further analysis
+
+         osprey <- osprey_raw %>%
+                     dplyr::select("timestamp", "location.long", "location.lat", "external.temperature", 
+                                   "gsm.gsm.signal.strength", "sensor.type", "individual.local.identifier")%>%
+                     rename("lon"="location.long",
+                            "lat"="location.lat",
+                            "ext_temp"="external.temperature",
+                            "gsm_signal_strength"="gsm.gsm.signal.strength",
+                            "sensor_type"="sensor.type",
+                            "id"="individual.local.identifier")%>%
+                     mutate(id = as.factor(id),
+                            time = as.POSIXct(timestamp, tz = "UTC"),
+                            signal_interruption_cause = ifelse (id %in% osprey_dead, "Death", "GPS lifecycle"),
+                            date = as_date(parse_date_time(timestamp, orders = c("%Y-%m-%d %H:%M:%S"))),
+                            day = day(time),
+                            month = month(time),
+                            year = year(time))%>%
+                     unite(m_day, c(month, day), sep="/", remove = F)%>%
+                     left_join(osprey_death, by = c("id" = "id"))%>%
+                     mutate( season = case_when(
+                                month %in% 10:12 ~ "Fall",
+                                month %in%  1:3  ~ "Winter",
+                                month %in%  4:6  ~ "Spring",
+                                TRUE ~ "Summer"),
+                             ID = case_when(
+                             id == "Balearics2013_FOSP11-Juv_ringH7" ~ "H7",
+                             id == "Corsica2013_FOSP17_Juv_ringCBK" ~ "CBK",
+                             id == "Corsica2014_FOSP21-Juv_ringCIV" ~ "CIV",
+                             id == "Italy2014_FOSP27-Juv_ringE7_Edy" ~ "E7",
+                             id == "Italy2015_FOSP30-juv-ringA7-Cook" ~ "A7",
+                             id == "Italy2015_FOSP33-juv-Antares" ~ "Antares",
+                             id == "Italy2016_FOSP28_juv_ringIAD_Ciccia" ~ "IAD",
+                             id == "Italy2016_FOSP37-adult-ringCAM Mora" ~ "CAM",
+                             id == "Italy2017_FOSP43_juv_ringIBI_Agrippa" ~ "IBI",
+                             id == "Italy2018_FIOS45_juv_ringIAB_Anassagora" ~ "IAB",
+                             id == "Italy2019_OrnitelaWhite_juv_ringICZ_Odaba" ~ "ICZ",
+                             id == "Italy2020_FIOS21_juv_ringIBS_Mauna Loa" ~ "IBS",
+                             id == "Italy2020_Ornitela_juv_ringIBH_Infiernillo" ~ "IBH",
+                             id == "Italy2020_Ornitela_juv_ringIBK_Imbabura" ~ "IBK"))
+
+         osprey <- osprey%>%
+         select(-c("timestamp", "id"))%>%
+         relocate("ID", "time", "date", "day", "month", "year", "m_day",
+                  "death_date", "season", "ext_temp", "lon", "lat", "sensor_type", "gsm_signal_strength", "signal_interruption_cause", "death_comment")%>%
+         select(-c("death_comment", "ext_temp", "sensor_type", "gsm_signal_strength"))%>%
+         unique()
+
+
+
+
+         # Estimating movement rate
+
+                  crdref <- "+proj=longlat +datum=WGS84"
+                  osprey_shp <- vect(osprey, crs=crdref)
+                  osprey_df <- as.data.frame(osprey_shp)
+
+
+                  getMoveStats <- function(df){
+                    # df - is a generic data frame that will contain X,Y and Time columns
+                    Z <- df$lon + 1i*df$lat
+                    Time <- df$time
+                    Step <- c(NA, diff(Z)) # we add the extra NA because there is no step to the first location
+                    dT <- c(NA, difftime(Time[-1], Time[-length(Time)], hours) %>% as.numeric)
+
+                    SL <- Mod(Step)/1e3 # convert to km - so the speed is in km/hour
+                    MR <- SL/dT # computing the movement rate
+
+                    # this is what the function returns
+                    data.frame(df, Z, dT, Step, SL, MR)
+                  }
+
+
+                  osprey_move <- getMoveStats(osprey) 
+                                    
+                  ggplot(osprey_move, aes(ID, MR)) +
+                           geom_boxplot()
+
+
+                  mr_summarystats <- osprey_move %>%
+                           plyr::ddply(c("ID", "season"), summarize,
+                            min = min(MR, na.rm = TRUE), max = max(MR, na.rm = TRUE),
+                            n = length(MR), NA.count = sum(is.na(MR)),
+                            Zero.count = sum(MR == 0, na.rm = TRUE))
