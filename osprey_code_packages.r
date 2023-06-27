c8547a12a46ea0049b9afa1bd246bd089dc8f4b8
FedericoTossani <fede.tossani@gmail.com>
2023-06-27 14:21:42 +0200

    Update osprey_code_packages.r

--- osprey_code_packages.r
+++ osprey_code_packages.r
@@ -1,3 +1,25 @@
 # this is the list of all packages required in this project
 
+         list.of.packages <- c("tidyverse",
+                               "lubridate",
+                               "sf", 
+                               "mapview", 
+                               "adehabitatLT",
+                               "adehabitatHR",
+                               "stargazer",
+                               "kableExtra",
+                               "ggmap",
+                               "terra",
+                               "tidyterra",
+                               "gridExtra",
+                               "ggpubr")
 
+         # with this line of code I check if alll the packages are installed and then I load it
+
+         {
+           new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+           if(length(new.packages)) install.packages(new.packages)
+
+           lapply(list.of.packages, require, character.only = TRUE)
+         }
