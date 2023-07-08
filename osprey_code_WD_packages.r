183cd9efc3a758f27af5c81f574213c2c1b5b6ec
FedericoTossani <fede.tossani@gmail.com>
2023-07-08 11:53:47 +0200

    Update osprey_code_WD_packages.r

--- osprey_code_WD_packages.r
+++ osprey_code_WD_packages.r
@@ -17,7 +17,12 @@
                                "gridExtra",
                                "ggpubr",
                                "DescTools",
-                               "devtools")
+                               "devtools",
+                               "moveHMM",
+                               "momentuHMM",
+                               "foieGras",
+                               "crawl",
+                               "ggspatial")
 
          # with this line of code I check if alll the packages are installed and then I load it
 
