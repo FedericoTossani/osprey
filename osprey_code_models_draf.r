db2655aaf78b7d029e86e08d9afa5822be13647d
FedericoTossani <fede.tossani@gmail.com>
2023-07-08 11:54:39 +0200

    Update osprey_code_models_draf.r

--- osprey_code_models_draf.r
+++ osprey_code_models_draf.r
@@ -1,143 +1,30 @@
 
+
+# =============================== #
+#       1.SetWD & packages       #
+# =============================== #
+
+# Here you will load all the packages needed and also the working directory
+          source("https://raw.githubusercontent.com/FedericoTossani/osprey/main/osprey_code_WD_packages.r")
+
+# =========================================== #
+#          2.Data import & selection          #
+# =========================================== #
+
+# Here you will load the raw dataset and the filtered one 
+          source("https://raw.githubusercontent.com/FedericoTossani/osprey/main/osprey_code_data_import_selection.r")
+
+# ===================================== #
+#          3.Basemaps & extent          #
+# ===================================== #
+
+# Here you will load all the maps needed and also the cropped geographic extent for all individual ospreys
+          source("https://raw.githubusercontent.com/FedericoTossani/osprey/main/osprey_code_basemap_extent.r")
+
+
+
+
 # Experiments to categorize fixes with HMM
-
-setwd("C:/Tesi/R/osprey/data/")
-
-list.of.packages <- c("tidyverse",
-                      "lubridate",
-                      "sf", 
-                      "mapview", 
-                      "adehabitatLT",
-                      "adehabitatHR",
-                      "stargazer",
-                      "kableExtra",
-                      "ggmap",
-                      "terra",
-                      "tidyterra",
-                      "moveHMM",
-                      "momentuHMM",
-                      "foieGras",
-                      "crawl",
-                      "gridExtra",
-                      "ggspatial")
-
-# with this line of code I check if alll the packages are installed and then I load it
-
-{
-  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-  
-  if(length(new.packages)) install.packages(new.packages)
-  
-  lapply(list.of.packages, require, character.only = TRUE)
-}
-
-
-# Load funcitons for later
-# source("https://github.com/FedericoTossani/osprey/blob/main/utility_function.r")  # this doesn't work
-
-
-# ================================= #
-          ## Data import ##
-# ================================= #
-
-list_csv <- list.files(pattern = "20")
-csv_allfile <- lapply(list_csv, read.csv)
-
-col_selected <- c("timestamp", "location.long", "location.lat", "external.temperature", 
-                  "gsm.gsm.signal.strength", "sensor.type", "individual.local.identifier")
-
-csv_file_sel_col <- lapply(csv_allfile, "[", , col_selected)
-osprey_raw <- bind_rows(csv_file_sel_col)
-
-osprey_death <- read.csv("osprey_death.csv")
-
-
-# ================================= #
-         ## Filtering data ##
-# ================================= #
-
-# dead ospreys
-
-osprey_dead <- c("Balearics2013_FOSP11-Juv_ringH7", "Corsica2013_FOSP17_Juv_ringCBK",
-                 "Italy2017_FOSP43_juv_ringIBI_Agrippa", "Italy2018_FIOS45_juv_ringIAB_Anassagora")
-
-# dataset to use for further analysis
-
-osprey <- osprey_raw %>%
-            dplyr::select("timestamp", "location.long", "location.lat", "external.temperature", 
-                          "gsm.gsm.signal.strength", "sensor.type", "individual.local.identifier")%>%
-            rename("lon"="location.long",
-                   "lat"="location.lat",
-                   "ext_temp"="external.temperature",
-                   "gsm_signal_strength"="gsm.gsm.signal.strength",
-                   "sensor_type"="sensor.type",
-                   "id"="individual.local.identifier")%>%
-            mutate(id = as.factor(id),
-                   time = as.POSIXct(timestamp, tz = "UTC"),
-                   signal_interruption_cause = ifelse (id %in% osprey_dead, "Death", "GPS lifecycle"),
-                   date = as_date(parse_date_time(timestamp, orders = c("%Y-%m-%d %H:%M:%S"))),
-                   day = day(time),
-                   month = month(time),
-                   year = year(time))%>%
-            unite(m_day, c(month, day), sep="/", remove = F)%>%
-            left_join(osprey_death, by = c("id" = "id"))%>%
-            mutate( season = case_when(
-                       month %in% 10:12 ~ "Fall",
-                       month %in%  1:3  ~ "Winter",
-                       month %in%  4:6  ~ "Spring",
-                       TRUE ~ "Summer"),
-                    ID = case_when(
-                    id == "Balearics2013_FOSP11-Juv_ringH7" ~ "H7",
-                    id == "Corsica2013_FOSP17_Juv_ringCBK" ~ "CBK",
-                    id == "Corsica2014_FOSP21-Juv_ringCIV" ~ "CIV",
-                    id == "Italy2014_FOSP27-Juv_ringE7_Edy" ~ "E7",
-                    id == "Italy2015_FOSP30-juv-ringA7-Cook" ~ "A7",
-                    id == "Italy2015_FOSP33-juv-Antares" ~ "Antares",
-                    id == "Italy2016_FOSP28_juv_ringIAD_Ciccia" ~ "IAD",
-                    id == "Italy2016_FOSP37-adult-ringCAM Mora" ~ "CAM",
-                    id == "Italy2017_FOSP43_juv_ringIBI_Agrippa" ~ "IBI",
-                    id == "Italy2018_FIOS45_juv_ringIAB_Anassagora" ~ "IAB",
-                    id == "Italy2019_OrnitelaWhite_juv_ringICZ_Odaba" ~ "ICZ",
-                    id == "Italy2020_FIOS21_juv_ringIBS_Mauna Loa" ~ "IBS",
-                    id == "Italy2020_Ornitela_juv_ringIBH_Infiernillo" ~ "IBH",
-                    id == "Italy2020_Ornitela_juv_ringIBK_Imbabura" ~ "IBK"))
-              
-osprey <- osprey%>%
-        select(-c("timestamp", "id"))%>%
-        relocate("ID", "time", "date", "day", "month", "year", "m_day",
-                             "death_date", "season", "ext_temp", "lon", "lat", "sensor_type", "gsm_signal_strength", "signal_interruption_cause", "death_comment")%>%
-         unique()
-
-
-# subsetting only the columns we need
-
-data <- osprey%>%
-      dplyr::select(ID, time, lon, lat)
-
-llcoord <- st_as_sf(data[, c("lon", "lat")], coords = c("lon", "lat"),
-crs = CRS("+proj=longlat +datum=WGS84"))
-utmcoord <- st_transform(llcoord, crs = CRS("+proj=utm +zone=32 +datum=WGS84"))
-
-# Add Easting-Northing to data (in km)
-data[, c("x", "y")] <- st_coordinates(utmcoord)/1000
-
-
-# Create an ltraj object
-
-data2 <- data %>%
-  group_by(ID, time) %>%
-  distinct() %>%
-  ungroup()
-
-dupl <- which(duplicated(data$time))
-
-time <- as.POSIXct(strptime(as.character(data2$time),"%Y-%m-%d %H:%M:%S"))
-
-
-data_lt <- as.ltraj(xy = data2[,c("x","y")], date = time, id = data2$ID)
-
-
-
 
 
 # visualize the data set
