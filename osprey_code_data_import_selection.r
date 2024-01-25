379c39262331e8bbd29883385127fa73d14fd817
FedericoTossani <fede.tossani@gmail.com>
2024-01-25 10:08:47 +0100

    Update osprey_code_data_import_selection.r

--- osprey_code_data_import_selection.r
+++ osprey_code_data_import_selection.r
@@ -107,7 +107,7 @@ nd_df <- osprey%>%
                   select(-c('day', 'month', 'year', 'm_day', 'ext_temp', 'signal_interruption_cause', 'death_comment'))%>%
                   unique()
 
-nd_df <- osprey%>%
+nd_df <- nd_df%>%
                     filter( ID == 'A7' & time >= '2015-08-14 08:00:00' & time <= '2015-08-18 09:00:00' |
                      ID == 'A7' & time >= '2017-02-20 06:00:00' & time <= '2017-03-01 12:00:00' |
                      ID == 'A7' & time >= '2017-03-17 06:00:00' & time <= '2017-04-14 18:00:00'  |
